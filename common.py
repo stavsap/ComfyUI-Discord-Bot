a7e0f0f0039e125776571ebfce043520d9f92d24
Stav Sapir <stavsap@gmail.com>
2024-03-29 13:59:36 +0300

    refactoring
    Signed-off-by: Stav Sapir <stavsap@gmail.com>

--- common.py
+++ common.py
@@ -1,10 +1,10 @@
 import logging
 
 def get_logger(name, level=logging.INFO):
-    _logger = logging.getLogger(name)
-    _c_handler = logging.StreamHandler()
-    _formatter = logging.Formatter('%(asctime)s [%(levelname)s][%(name)s]: %(message)s')
-    _c_handler.setFormatter(_formatter)
-    _logger.addHandler(_c_handler)
-    _logger.setLevel(level)
-    return _logger
+    logger = logging.getLogger(name)
+    c_handler = logging.StreamHandler()
+    formatter = logging.Formatter('%(asctime)s [%(levelname)s][%(name)s]: %(message)s')
+    c_handler.setFormatter(formatter)
+    logger.addHandler(c_handler)
+    logger.setLevel(level)
+    return logger
